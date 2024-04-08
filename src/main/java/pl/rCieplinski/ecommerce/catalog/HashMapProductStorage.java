058a978b51df2a595aa83b7daea2efbbe9c6537d
s-301-27 <s-301-27@gumiforest.uek.krakow.pl>
2024-04-08 16:33:16 +0200

    Another class

---
+++ src/main/java/pl/rCieplinski/ecommerce/catalog/HashMapProductStorage.java
@@ -0,0 +1,22 @@
+package pl.rCieplinski.ecommerce.catalog;
+
+import java.util.List;
+
+public class HashMapProductStorage implements ProductStorage {
+    HashMap<String, Product> products;
+
+    @Override
+    public List<Product> allProducts() {
+        return products.values().stream().toList();
+    }
+
+    @Override
+    public void add(Product newProduct) {
+        products.put(product.getId(), product);
+    }
+
+    @Override
+    public Product getProductBy(String id) {
+        return null;
+    }
+}
---
+++ src/test/java/pl/rCieplinski/ecommerce/catalog/HashMapProductStorageTest.java
@@ -0,0 +1,39 @@
+package pl.rCieplinski.ecommerce.catalog;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.*;
+public class HashMapProductStorageTest {
+    private static final String TEST_PRODUCT_NAME = "test product";
+
+    @Test
+    void isStoreNewProduct(){
+        ProductStorage storage = thereIsProductStorage();
+        Product product = thereIsExampleProduct();
+        storage.add(product);
+        List<Product> products = storage.allProducts();
+        assertThat(products).hasSize(1).extracting(Product::getName).contains(TEST_PRODUCT_NAME);
+
+    }
+
+    private Product thereIsExampleProduct() {
+        return new Product(UUID.randomUUID(), TEST_PRODUCT_NAME, "my des");
+    }
+
+    private Product thereIsProductStorage() {
+        return new HashMapProductStorage();
+
+    }
+
+    @Test
+    void itLoadsAllProducts(){
+
+    }
+    @Test
+    void itLoadsProductById(){
+
+    }
+}
